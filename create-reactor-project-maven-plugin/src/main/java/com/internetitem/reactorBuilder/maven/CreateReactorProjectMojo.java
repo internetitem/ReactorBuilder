7f0f77ea3da3eb8a16db62488bd2d420a9058ff5
Adam Batkin <adam@batkin.net>
2015-11-19 23:10:52 -0500

    Increase version, change parent ID, change configuration of mojo

--- create-reactor-project-maven-plugin/src/main/java/com/internetitem/reactorBuilder/maven/CreateReactorProjectMojo.java
+++ create-reactor-project-maven-plugin/src/main/java/com/internetitem/reactorBuilder/maven/CreateReactorProjectMojo.java
@@ -10,21 +10,52 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-@Mojo(name = "create-reactor-project", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, threadSafe = true)
+@Mojo(name = "create-reactor-project", defaultPhase = LifecyclePhase.GENERATE_RESOURCES, threadSafe = true, requiresProject = false)
 public class CreateReactorProjectMojo extends AbstractMojo {
 
-	/**
-	 * Reactor configuration
-	 */
-	@Parameter(property = "reactorConfiguration", required = false)
-	private Configuration reactorConfiguration;
+	private static final String PROPERTY_PREFIX = "create-reactor.";
+
+	@Parameter(property = "xmlns", required = false, defaultValue = "${" + PROPERTY_PREFIX + "xmlns}")
+	private String xmlns = "http://maven.apache.org/POM/4.0.0";
+
+	@Parameter(property = "templateFile", required = false, defaultValue = "${" + PROPERTY_PREFIX + "templateFile}")
+	private String templateFile;
+
+	@Parameter(property = "groupId", required = false, defaultValue = "${" + PROPERTY_PREFIX + "groupId}")
+	private String groupId;
+
+	@Parameter(property = "artifactId", required = false, defaultValue = "${" + PROPERTY_PREFIX + "artifactId}")
+	private String artifactId;
+
+	@Parameter(property = "version", required = false, defaultValue = "${" + PROPERTY_PREFIX + "version}")
+	private String version;
+
+	@Parameter(property = "packaging", required = false, defaultValue = "${" + PROPERTY_PREFIX + "packaging}")
+	private String packaging = "pom";
+
+	@Parameter(property = "prependModules", required = false, defaultValue = "${" + PROPERTY_PREFIX + "prependModules}")
+	private String prependModules;
+
+	@Parameter(property = "appendModules", required = false, defaultValue = "${" + PROPERTY_PREFIX + "appendModules}")
+	private String appendModules;
+
+	@Parameter(property = "relativeTo", required = false, defaultValue = "${" + PROPERTY_PREFIX + "relativeTo}")
+	private String relativeTo;
+
+	@Parameter(property = "moduleSearchDirectories", required = false, defaultValue = "${" + PROPERTY_PREFIX + "moduleSearchDirectories}")
+	private String moduleSearchDirectories;
+
+	@Parameter(property = "outputFile", required = false, defaultValue = "${" + PROPERTY_PREFIX + "outputFile}")
+	private String outputFile;
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		if (reactorConfiguration == null) {
-			reactorConfiguration = new Configuration();
-		}
+		Configuration reactorConfiguration = buildReactorConfiguration();
+
 		Inputter inputter = new FileInputter();
 		Outputter outputter = new FileOutputter();
 		ModuleLister lister = new FilesystemModuleLister();
@@ -34,5 +65,48 @@ public class CreateReactorProjectMojo extends AbstractMojo {
 		} catch (IOException e) {
 			throw new MojoExecutionException("Failed to generate Reactor Project: " + e.getMessage(), e);
 		}
+	}
+
+	private Configuration buildReactorConfiguration() {
+		Configuration config = new Configuration();
+
+		if (xmlns != null) {
+			config.setXmlns(xmlns);
+		}
+		if (templateFile != null) {
+			config.setTemplateFile(templateFile);
+		}
+		if (groupId != null) {
+			config.setGroupId(groupId);
+		}
+		if (artifactId != null) {
+			config.setArtifactId(artifactId);
+		}
+		if (version != null) {
+			config.setVersion(version);
+		}
+		if (packaging != null) {
+			config.setPackaging(packaging);
+		}
+		if (prependModules != null && !prependModules.isEmpty()) {
+			List<String> prependModulesList = Arrays.asList(prependModules.split(":"));
+			config.setPrependModules(prependModulesList);
+		}
+		if (appendModules != null && !appendModules.isEmpty()) {
+			List<String> appendModulesList = Arrays.asList(appendModules.split(":"));
+			config.setAppendModules(appendModulesList);
+		}
+		if (relativeTo != null) {
+			config.setRelativeTo(relativeTo);
+		}
+		if (moduleSearchDirectories != null && !moduleSearchDirectories.isEmpty()) {
+			List<String> moduleSearchDirectoriesList = Arrays.asList(moduleSearchDirectories.split(":"));
+			config.setModuleSearchDirectories(moduleSearchDirectoriesList);
+		}
+		if (outputFile != null) {
+			config.setOutputFile(outputFile);
+		}
+
+		return config;
 	}
 }
